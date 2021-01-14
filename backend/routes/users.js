d8a9a668f0a61ec9bfb1916051d2a8d325885b58
vjspranav <pranavasri@live.in>
2021-01-14 19:31:10 +0530

    Add intial signup and login api * Add support for prettier and eslint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,57 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+// Load User model
+const User = require("../models/users");
+
+// POST request
+// Add a user to db
+router.post("/register", (req, res) => {
+    var today = new Date();
+    const newUser = new User({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        date: req.body.date ? req.body.date : today,
+    });
+
+    newUser
+        .save()
+        .then((user) => {
+            res.status(200).json(user);
+        })
+        .catch((err) => {
+            res.status(400).send(err);
+        });
+});
+
+// POST request
+// Login
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const username = req.body.username;
+    const pass = req.body.password;
+    // Find user by email
+    User.findOne({ email, username }).then((user) => {
+        // Check if user email exists
+        if (!user) {
+            return res.status(404).json({
+                error: "Email not found",
+            });
+        } else {
+            if (pass === user.password) {
+                res.status(200).json({
+                    details: "Login Successfull",
+                });
+                return user;
+            } else {
+                return res.status(404).json({
+                    error: "Password wrong",
+                });
+            }
+        }
+    });
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -33,36 +33,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Connect to moongose
 // Connection to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/" + DB_NAME, {
-  useNewUrlParser: true,
+    useNewUrlParser: true,
 });
 const connection = mongoose.connection;
-connection.once("open", function () {
-  console.log(
-    `MongoDB database connection to db ${DB_NAME} established successfully !`
-  );
+connection.once("open", function() {
+    console.log(
+        `MongoDB database connection to db ${DB_NAME} established successfully !`
+    );
 });
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
 module.exports = app;
 
-app.listen(PORT, function () {
-  console.log("Server is running on Port: " + PORT);
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
 });
