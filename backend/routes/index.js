0f92c2486ea7b7ade41f9e924c1bb44e832da554
vjspranav <pranavasri@live.in>
2021-01-19 02:25:56 +0530

    Add a protected page to see if it works or not

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-    res.render("index", { title: "Express" });
+router.get("/", function (req, res) {
+  res.send("API is working properly !");
+});
+
+/* Protected GET home page. */
+router.get("/protected", function (req, res) {
+  if (!req.session.user)
+    return res.status(500).json({ status: "Not logged in" });
+  res.send("API atfer login is working properly !");
 });
 
 module.exports = router;
